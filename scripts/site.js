fcbc2a512aa4630b85029654495b9b73d04b3350
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2012-06-24 10:35:26 -0500

    Remove old css and scripts to do the redesign.

--- scripts/site.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $("body .main .big-list-holder .clickable").click(function(e) {
-    e.preventDefault();
-    location.href = $('article h2 a', e.currentTarget).attr('href');
-  });
-});
